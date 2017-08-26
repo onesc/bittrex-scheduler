d4878910c63d22a017f6bcb8f2b148c2dbb6354b
onesc <onescyt@gmail.com>
2017-08-27 07:05:17 +1000

    iterating through conditional trades

--- app/app.js
+++ app/app.js
@@ -4,27 +4,32 @@ const api = require ('./api.js');
 const equal = require('deep-equal');
 
 const performConditionalTrade = condition => new Promise(async (resolve, reject) => {
-    const status = await api.checkOrderStatus(condition.uuid).catch((err) => { reject(err) });
-    if (!status.isOpen && !status.CancelInitiated) {
-        const trade = await api.makeBittrexOrder(condition.options).catch((err) => { reject(err) });
-        if (trade && trade.success) {
-            removeCondition(condition);
-
-            if (condition.nextCondition) {
-                condition.nextCondition.uuid = trade.result.uuid;
-                pushCondition(condition.nextCondition) 
-            } 
-        };
-    } 
+    try {    
+        const status = await api.checkOrderStatus(condition.uuid).catch((err) => { reject(err) });
+        if (!status.isOpen && !status.CancelInitiated) {
+            const trade = await api.makeBittrexOrder(condition.options).catch((err) => { reject(err) });
+            if (trade && trade.success) {
+                iterateCondition(condition, trade.uuid);
+            };
+        } 
+    } catch (err) { console.error(err) }
 });   
 
-const removeCondition = (condition, all = false) => {
+const iterateCondition = (condition, uuid) => {
     fs.readFile(path.resolve(__dirname + "/conditions.json"), async (err, data) => {
         if (err) console.error(err);
         const pendingTrades = JSON.parse(data)
+
         const filteredTrades = pendingTrades.map((p) => {
+            console.log(!equal(p, condition))
+            console.log
             if (!equal(p, condition)) return p;
-        }).filter((item) => item)
+            if (p.nextCondition) {
+                p.nextCondition.uuid = condition.uuid;
+                return p.nextCondition 
+            }
+        }).filter((item) => item);
+
         fs.writeFileSync(path.resolve(__dirname + "/conditions.json"), JSON.stringify(filteredTrades, null, '\t'))   
     })
 }
@@ -40,38 +45,67 @@ const pushCondition = condition => {
 
 const trade = async () => {
     try {
-        const tradeOptions = { 
+        const initialTradeOptions = { 
             market: 'BTC-OMG', 
-            quantity: 0.5, 
-            rate: 0.00188270, 
+            quantity: 0.3, 
+            rate: 0.00187, 
             buyOrSell: 'sell' 
         }
+        const tradeResult = await api.makeBittrexOrder(initialTradeOptions).catch((err) => { console.error("makeBittrexOrder rejected " + JSON.stringify(initialTradeOptions, null, '\trade')); console.error(err) });
 
-        const conditionalTradeOptions = { market: 'BTC-OMG', quantity: 0.5, rate: 0.00185744, buyOrSell: 'buy' }
-
-        const tradeResult = await api.makeBittrexOrder(tradeOptions).catch((err) => { console.error("makeBittrexOrder rejected " + JSON.stringify(tradeOptions, null, '\trade')); console.error(err) });
-
-        const condition = {
-            uuid: tradeResult.result.uuid, 
-            options: conditionalTradeOptions, 
-            originalOptions: tradeOptions, 
+        const fourthCondition = {
+            options: { 
+                market: 'BTC-OMG', 
+                quantity: 0.3, 
+                rate: 0.0018696, 
+                buyOrSell: 'sell' 
+            }, 
             nextCondition: false
         }
 
-        pushCondition(condition)
+        const thirdCondition = {
+            options: { 
+                market: 'BTC-OMG', 
+                quantity: 0.3, 
+                rate: 0.0018697, 
+                buyOrSell: 'sell' 
+            }, 
+            nextCondition: fourthCondition
+        }
+
+        const secondCondition = {
+            options: { 
+                market: 'BTC-OMG', 
+                quantity: 0.3, 
+                rate: 0.0018698, 
+                buyOrSell: 'sell' 
+            }, 
+            nextCondition: thirdCondition
+        }
+
+        const firstCondition = {
+            uuid: tradeResult.result.uuid, 
+            options: { 
+                market: 'BTC-OMG', 
+                quantity: 0.3, 
+                rate: 0.0018699, 
+                buyOrSell: 'sell' 
+            }, 
+            nextCondition: secondCondition
+        }
+
+        pushCondition(firstCondition)
     } catch(err) {
         console.error(err)
     }
     
 }
-// console.log(process.env.BKEY)
+
 // trade();
+// fs.readFile(path.resolve(__dirname + "/conditions.json"), async (err, data) => {
+//     if (err) console.error(err);
+//     const pendingTrades = JSON.parse(data)
+//     performConditionalTrade(pendingTrades[0]).catch((err) => { console.error(err) })
+// })
 
-fs.readFile(path.resolve(__dirname + "/conditions.json"), async (err, data) => {
-        if (err) console.error(err);
-        const pendingTrades = JSON.parse(data)
-        performConditionalTrade(pendingTrades[0]).catch((err) => { console.error(err) })
-    })
-
-// performConditionalTrade()
 
